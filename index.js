2f239fc7412ec63db3da50dff9da6d5523537241
Sebastian Skov <dashxgraphics@gmail.com>
2022-01-20 17:28:07 +0100

    Fixed nav

---
+++ index.js
@@ -0,0 +1,26 @@
+const sections = document.querySelectorAll("section");
+const navLi = document.querySelectorAll(".navlink");
+window.onscroll = () => {
+  var current = "";
+  var currentScrollPos = window.pageYOffset;
+
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    if (pageYOffset >= sectionTop - 60) {
+      current = section.getAttribute("id"); }
+  });
+
+  navLi.forEach((li) => {
+    li.classList.remove("current");
+    if (li.classList.contains(current)) {
+      li.classList.add("current");
+    }
+  });
+
+  if (currentScrollPos > 100) {
+    document.getElementById('navbar').classList.add('scrolled');
+  } else if (currentScrollPos < 100) {
+    document.getElementById('navbar').classList.remove('scrolled')
+  }
+  prevScrollpos = currentScrollPos;
+};
